bb3c68746cb64b0cd2bd05aac60454da6bd3b01c
Vijay Krishnavanshi <vijaykrishnavanshi@gmail.com>
2024-05-26 12:35:59 +0530

    Minor Fiz

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -74,7 +74,7 @@ export const Header = () => {
         <Link href="/" passHref className="hidden lg:flex items-center gap-2 ml-4 mr-6 shrink-0">
           <div className="flex flex-col">
             <span className="font-bold leading-tight">Community Identity</span>
-            <span className="text-xs">ZuPass to ERC4337</span>
+            <span className="text-xs">Onchain Identity for ZuPass Groups</span>
           </div>
         </Link>
         <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">
